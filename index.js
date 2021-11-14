68a1165b83a46d84d372b75a98f3271a3b66289e
Nelo Puchades Bresó <correo@nelo.dev>
2021-11-14 14:12:00 +0100

    First route of server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('my app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
