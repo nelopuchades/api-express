3d875ad1f94f5ac04c50d3a55a775a6450806d67
Nelo Puchades Bresó <correo@nelo.dev>
2021-11-14 20:30:17 +0100

    Added /api/v1 to api endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 
 function setupRoutes(app) {
-    app.use('/products', productsRouter);
-    app.use('/users', usersRouter);
+    const router = express.Router();
+    app.use('/api/v1', router);
+    router.use('/products', productsRouter);
+    router.use('/users', usersRouter);
 }
 
 module.exports = setupRoutes;
