39d8516e9f7e048e7b944de424bfef717f3d6f3b
Nelo Puchades Bresó <correo@nelo.dev>
2021-11-14 14:54:39 +0100

    Playing with GET endpoint - Params and Query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker');
+
 const app = express();
 const port = 3000;
 
@@ -11,10 +13,83 @@ app.get('/new-route', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-    res.json({
-        name: 'Coca Cola',
-        price: 1000
-    });
+    const products = [];
+    const { size } = req.query;
+    const limit = size || 10;
+
+    for(let i = 0; i < limit; i++) {
+        products.push({
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.price(), 10),
+            image: faker.image.imageUrl(),
+        });
+    }
+
+    res.json(products);
+});
+
+app.get('/products/:productId', (req, res) => {
+    const { productId } = req.params;
+    const products = [
+        {
+            id: 1,
+            name: 'Coca Cola',
+            price: 1000
+        },
+        {
+            id: 2,
+            name: 'Fanta',
+            price: 1500
+        }
+    ];
+
+
+    const product = products.find(prod => prod.id === parseInt(productId));
+    
+    res.json(product);
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId } = req.params;
+    const products = [
+        {
+            id: 1,
+            name: 'Coca Cola',
+            category: 'Drink',
+            price: 1000
+        },
+        {
+            id: 2,
+            name: 'Fanta',
+            category: 'Beberage',
+            price: 1500
+        }
+    ];
+
+    const product = products.find(prod => prod.id === parseInt(productId) && prod.category === categoryId);
+    if (!product) {
+        res.json({
+            error: true,
+            message: `Couldn't find product with id ${productId} and category ${categoryId}`,
+        });
+    } else {
+        res.json(product);
+    }
+});
+
+app.get('/users', (req, res) => {
+    const { limit, offset } = req.query;
+    if (limit && offset) {
+        res.json({
+            limit,
+            offset,
+        });
+    } else {
+        res.json({
+            error: true,
+            message: 'There are no params to get',
+        });
+    }
 });
 
 app.listen(port, () => {
